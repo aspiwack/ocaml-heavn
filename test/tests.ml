4c7016d7b52fd691e714bad863921a8255acc6c1
Arnaud Spiwack <arnaud@spiwack.net>
2016-10-02 20:35:51 +0200

    Tests: use ounit

--- test/tests.ml
+++ test/tests.ml
@@ -1,4 +1,12 @@
+open OUnit2
+
+let suite =
+  "Parser validation" >::: [
+    "Mini-test" >:: (fun _ -> ignore @@ Parsing.of_file "test/edn/test1.edn")
+  ]
+
 
 let () =
-  let _ = Parsing.of_file "test/edn/test1.edn" in
-  Format.printf "SUCCESS@."
+  run_test_tt_main @@ test_list [
+    suite;
+  ]
